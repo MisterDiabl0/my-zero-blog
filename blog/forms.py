5b1e8bd2194047acf03af5b06f3a4dc384081031
MisterDiabl0 <blackystalk272@gmail.com>
2019-12-05 01:52:45 +0300

    fixed errors

--- blog/forms.py
+++ blog/forms.py
@@ -1,4 +1,4 @@
-# <<<<<<< HEAD
+
 from django import forms
 
 from .models import Post
@@ -8,18 +8,3 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('title', 'text',)
-
-# # print
-# =======
-# from django import forms
-#
-# from .models import Post
-#
-# class PostForm(forms.ModelForm):
-#
-#     class Meta:
-#         model = Post
-#         fields = ('title', 'text',)
-#
-# # print
-# >>>>>>> e21252fb01dc74cf003eb0e2e9b22aab8a2dcd45
