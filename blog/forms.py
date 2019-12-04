a2cceb58bd5e340c902a6b4e039d6626a1abebd8
MisterDiabl0 <blackystalk272@gmail.com>
2019-12-04 04:32:26 +0300

    fixed merge conflict

--- blog/forms.py
+++ blog/forms.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+# <<<<<<< HEAD
 from django import forms
 
 from .models import Post
@@ -9,17 +9,17 @@ class PostForm(forms.ModelForm):
         model = Post
         fields = ('title', 'text',)
 
-# print
-=======
-from django import forms
-
-from .models import Post
-
-class PostForm(forms.ModelForm):
-
-    class Meta:
-        model = Post
-        fields = ('title', 'text',)
-
-# print
->>>>>>> e21252fb01dc74cf003eb0e2e9b22aab8a2dcd45
+# # print
+# =======
+# from django import forms
+#
+# from .models import Post
+#
+# class PostForm(forms.ModelForm):
+#
+#     class Meta:
+#         model = Post
+#         fields = ('title', 'text',)
+#
+# # print
+# >>>>>>> e21252fb01dc74cf003eb0e2e9b22aab8a2dcd45
